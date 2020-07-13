2391facdc0f6e9f95b8e8b84eb33a31152789c09
k1574 <k1574@yandex.ru>
2020-07-13 06:50:06 +0500

    Init.

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *argv0;
+
+void
+usage(void)
+{
+	fprintf(stderr, "Usage: %s <first_char> <last_char>\n", argv0);	
+	exit(1);
+}
+
+void
+main(int argc, char *argv[])
+{
+	char c1, c2;
+	argv0 = argv[0] ;
+	if(argc!=3 || argv[1][1]!=0 || argv[2][1]!=0 )
+		usage();
+	c1 = *argv[1] ;
+	c2 = *argv[2] ;
+	while(c1<=c2)
+		printf("%c", c1++);
+	puts("");
+	exit(0);
+}
